bdc3dce646597992f34c6f248f5009a720c4b63a
Hugh <hugh@crablab.co.uk>
2019-10-21 17:40:33 +0100

    Borrowing some example code that largely works as debugging this is getting frustrating: https://stackoverflow.com/a/56632812

--- proof_of_concept_programs/clicker_emulator/clicker_send.py
+++ proof_of_concept_programs/clicker_emulator/clicker_send.py
@@ -3,10 +3,32 @@ import threading
 
 conn = serial.Serial('/dev/ttyUSB1', 115200)
 
-def serial_read():
+# Some example code: https://stackoverflow.com/a/56632812
+class ReadLine:
+    def __init__(self, s):
+        self.buf = bytearray()
+        self.s = s
+
+    def readline(self):
+        i = self.buf.find(b"\n")
+        if i >= 0:
+            r = self.buf[:i+1]
+            self.buf = self.buf[i+1:]
+            return r
+        while True:
+            i = max(1, min(2048, self.s.in_waiting))
+            data = self.s.read(i)
+            i = data.find(b"\n")
+            if i >= 0:
+                r = self.buf + data[:i+1]
+                self.buf[0:] = data[i+1:]
+                return r
+            else:
+                self.buf.extend(data)
+    
+def serial_read(sread):
     while True:
-        bytesToRead = conn.inWaiting()
-        conn.read(bytesToRead)
+        print(sread.readline())
 
 def serial_send():
     if(input()):
@@ -17,5 +39,10 @@ def serial_send():
         print("sent data")
 
 if __name__ == "__main__":
-    serial_read.start()
-    serial_send.start()
+    sread = ReadLine(conn)
+
+    read = threading.Thread(target=serial_read, args=[sread])
+    send = threading.Thread(target=serial_send)
+
+    read.start()
+    send.start()
