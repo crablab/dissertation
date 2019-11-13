7b92a62f5084524247262d1e4a9f7d07207787fd
Hugh <hugh@crablab.co.uk>
2019-11-13 22:27:16 +0000

    - Added missing assets - Readded required bibliography refrences - Fixed generation script

--- reports/interim/generate.sh
+++ reports/interim/generate.sh
@@ -1,1 +1,1 @@
-pandoc --filter pandoc-citeproc -B title.md interim.md-H preamble.tex --variable classoption=twocolumn --bibliography=interim.bib --output interim.md.pdf
+pandoc --filter pandoc-citeproc -B title.md declaration.md interim.md -H preamble.tex --variable classoption=twocolumn --bibliography=interim.bib --output interim.md.pdf
