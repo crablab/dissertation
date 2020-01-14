869726de275aac2496901bc44f050b4b17e71964
Hugh <hugh@crablab.co.uk>
2020-01-14 14:13:53 +0000

    Add a basic ping service

--- backend/app.py
+++ backend/app.py
@@ -4,3 +4,10 @@ from flask_cors import CORS
 # Flask Set Up
 app = Flask(__name__)
 CORS(app)
+
+@app.route("/ping")
+def handlePing():
+    return jsonify({"ping": "pong"})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
