c1bf952583999b54061fc067fadd7743c92c9f8b
Hugh <hugh@crablab.co.uk>
2020-02-17 19:27:23 +0000

    Add some documentation for the user class

--- backend/app/libraries/user.py
+++ backend/app/libraries/user.py
@@ -8,11 +8,20 @@ class user():
     """
 
     def __init__(self):
+        """
+        Instantiates user object.
+        """
         # Instantiate Database
         self.__database = instance = db()
         self.__db = instance.getInstance()
     
     def check_login(self, email, password):
+        """
+        Checks the email and password, returning True if they match. 
+
+        Note: this is a temporary function for testing.
+        """
+        cursor = self.__db.cursor()
         cursor.execute("SELECT * FROM `users` WHERE `email` = %s;", email)
         if cursor.rowcount == 1:
             user = cursor.fetchone()
