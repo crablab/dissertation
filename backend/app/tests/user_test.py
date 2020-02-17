6fa8d32352291d3112ed6a1414dee4a2744ea9dd
Hugh <hugh@crablab.co.uk>
2020-02-17 22:02:26 +0000

    Scope fixtures to a session, so they are executed only on instantiation!

--- backend/app/tests/user_test.py
+++ backend/app/tests/user_test.py
@@ -1,14 +1,25 @@
-import pytest
+import pytest, cuid
 
-@pytest.fixture
+@pytest.fixture(scope='session')
+def email():
+    id = cuid.CuidGenerator()
+    return id.cuid() + "@live.rhul.ac.uk"
+
+@pytest.fixture(scope='session')
 def user_class():
     from ..libraries import user
     return user.user()
 
-def test_create_student_user(user_class):
+def test_create_student_user(user_class, email):
     name = "Joe Bloggs"
-    email = "joe@live.rhul.ac.uk"
     password = "test"
     type = "student"
 
-    user_class.create_user(name, email, password, type)
+    assert user_class.create_user(name, email, password, type) != False
+
+def test_login_student_user(user_class, email):
+    password = "test"
+
+    assert user_class.check_login(email, password) == True
+
+
