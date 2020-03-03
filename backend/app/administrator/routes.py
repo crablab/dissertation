701b07f480126d0996b562b4a712e87b87a7c0f0
Hugh <hugh@crablab.co.uk>
2020-03-03 21:18:49 +0000

    Working (temp) user choices

--- backend/app/administrator/routes.py
+++ backend/app/administrator/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template, url_for, redirect, abort
 from flask_login import login_required, current_user
 from ..forms import AssignmentForm
+from ..libraries import users
 
 from . import administrator 
 
@@ -11,12 +12,27 @@ def index():
         abort(403) 
 
     ass_form = AssignmentForm()
-    configure_form(ass_form)
+    configure_assignment_form(ass_form)
 
     return render_template("administrator.html", form = ass_form)
 
-def configure_form(form):
-    form.user.choices = [("user1", "user1")]
+def configure_assignment_form(form):
+    """
+    Takes a flask_wtf object for the assignment form.
+    Loads it with the relevant data. 
+
+    :param form: the object for the assignment form
+    """
+    users_class = users.users()
+    users_class.load_users()
+    usr_choices = []
+
+    for key, value in users_class.users.items():
+        user = value.get_user()
+        usr_choices.append((key, user['name'] + " (" + user['email'] + ") - " + value.get_permissions))
+        # usr_choices.append((user.get_id, user.name + " (" + user.email + ") - " + user.get_permissions))
+
+    form.user.choices = usr_choices
     form.course.choices = [("course2", "course2")]
 
     return
