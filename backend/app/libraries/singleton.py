ce132169cc784df05901d7a165b39d1eff60b13f
Hugh <hugh@crablab.co.uk>
2020-03-03 00:39:51 +0000

    Add comments

--- backend/app/libraries/singleton.py
+++ backend/app/libraries/singleton.py
@@ -3,9 +3,16 @@
 class Singleton:
 
     def __init__(self, cls):
+        """
+        Instantiates Singleton object and sets internal class state.
+        :param cls: Class to be used in the Singleton 
+        """
         self._cls = cls
 
     def Instance(self):
+        """
+        Returns the Singleton's object.
+        """
         try:
             return self._instance
         except AttributeError:
@@ -13,7 +20,13 @@ class Singleton:
             return self._instance
 
     def __call__(self):
+        """
+        Provides an error if the class is called directly.
+        """
         raise TypeError('Singletons must be accessed through `Instance()`.')
 
     def __instancecheck__(self, inst):
+        """
+        Checks if an instance as an instance of the current internal class. 
+        """
         return isinstance(inst, self._cls)
