54fbdf7efc76b3c04dd5385e4a924a22e0bbae53
Hugh <hugh@crablab.co.uk>
2020-03-03 22:00:38 +0000

    Working form submission!

--- backend/app/administrator/routes.py
+++ backend/app/administrator/routes.py
@@ -1,8 +1,9 @@
-from flask import render_template, url_for, redirect, abort
+from flask import render_template, url_for, redirect, abort, flash
 from flask_login import login_required, current_user
 from ..forms import AssignmentForm
 from ..libraries import users
 from ..libraries import lectures
+from ..libraries import allocation
 
 from . import administrator 
 
@@ -14,6 +15,16 @@ def index():
 
     ass_form = AssignmentForm()
     configure_assignment_form(ass_form)
+
+    if ass_form.validate_on_submit():
+        # Create the allocation 
+        allocation_class = allocation.allocation()
+
+        if allocation_class.allocate(ass_form.user.data, ass_form.course.data) == True:
+            flash("Allocated successfully")
+        else:
+            flash("Allocation failed")
+        
 
     return render_template("administrator.html", form = ass_form)
 
@@ -42,7 +53,7 @@ def configure_assignment_form(form):
     course_choices = []
 
     for key, value in lectures_class.lectures.items():
-        course_choices.append((value.id, value.course))
+        course_choices.append((value.course, value.course))
     
     form.course.choices = course_choices
 
