7e3963ef1df22c87a1e3c3c14307a266991adfa6
Hugh <hugh@crablab.co.uk>
2020-03-03 21:32:37 +0000

    Display courses in form

--- backend/app/administrator/routes.py
+++ backend/app/administrator/routes.py
@@ -2,6 +2,7 @@ from flask import render_template, url_for, redirect, abort
 from flask_login import login_required, current_user
 from ..forms import AssignmentForm
 from ..libraries import users
+from ..libraries import lectures
 
 from . import administrator 
 
@@ -23,6 +24,7 @@ def configure_assignment_form(form):
 
     :param form: the object for the assignment form
     """
+    # Users
     users_class = users.users()
     users_class.load_users()
     usr_choices = []
@@ -33,6 +35,15 @@ def configure_assignment_form(form):
         # usr_choices.append((user.get_id, user.name + " (" + user.email + ") - " + user.get_permissions))
 
     form.user.choices = usr_choices
-    form.course.choices = [("course2", "course2")]
+
+    # Courses 
+    lectures_class = lectures.lectures()
+    lectures_class.load_distinct_courses()
+    course_choices = []
+
+    for key, value in lectures_class.lectures.items():
+        course_choices.append((value.id, value.course))
+    
+    form.course.choices = course_choices
 
     return
