286993fbdd71063613190e78a4fc3ce1ed832c11
Hugh <hugh@crablab.co.uk>
2020-03-10 17:40:38 +0000

    - Add table of contents - Add "outline" for discoverability

--- reports/final_report/generate.sh
+++ reports/final_report/generate.sh
@@ -1,1 +1,1 @@
-pandoc --filter pandoc-xnos --filter pandoc-citeproc -B title.md -B declaration.md final_report.md appendix.md -H preamble.tex --variable classoption=twocolumn --bibliography=final_report.bib --output final_report.md.pdf
+pandoc --toc --filter pandoc-xnos --filter pandoc-citeproc -B title.md -B declaration.md final_report.md appendix.md -H preamble.tex --variable classoption=twocolumn --bibliography=final_report.bib --output final_report.md.pdf
