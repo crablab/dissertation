a2709bb02903ae8430327d84727667603ff1a373
Hugh <hugh@crablab.co.uk>
2020-03-10 22:53:07 +0000

    Add the example WebUSB Arduino script.

---
+++ proof_of_concept_programs/web-usb/test_web_usb.ino
@@ -0,0 +1,21 @@
+// Example from the documentation: https://developers.google.com/web/updates/2016/03/access-usb-devices-on-the-web
+
+// Third-party WebUSB Arduino library
+#include <WebUSB.h>
+
+WebUSB WebUSBSerial(1 /* https:// */, "webusb.github.io/arduino/demos");
+
+#define Serial WebUSBSerial
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // Wait for serial port to connect.
+  }
+  Serial.write("WebUSB FTW!");
+  Serial.flush();
+}
+
+void loop() {
+  // Nothing here for now.
+}
