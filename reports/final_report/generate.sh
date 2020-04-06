5df97f3f8813caafd4e02e45d58e0a864f886c9d
Hugh <hugh@crablab.co.uk>
2020-04-06 21:02:30 +0100

    Change margins to fit tables

--- reports/final_report/generate.sh
+++ reports/final_report/generate.sh
@@ -1,1 +1,1 @@
-pandoc --toc --filter pandoc-xnos --filter pandoc-citeproc -B title.md -B declaration.md final_report.md appendix.md -H preamble.tex --bibliography=final_report.bib --output final_report.md.pdf
+pandoc --toc --filter pandoc-xnos --filter pandoc-citeproc -B title.md -B declaration.md final_report.md appendix.md -H preamble.tex -V geometry:margin=1in --bibliography=final_report.bib --output final_report.md.pdf
