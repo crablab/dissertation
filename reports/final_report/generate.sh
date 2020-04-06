373d13554962246c82e8b7f9a1bc5a15bc5d6976
Hugh <hugh@crablab.co.uk>
2020-04-06 21:00:12 +0100

    - Add table of results for fingerprinting investigation - Switch everything to one column for readability of figures and due to Pandoc limitations

--- reports/final_report/generate.sh
+++ reports/final_report/generate.sh
@@ -1,1 +1,1 @@
-pandoc --toc --filter pandoc-xnos --filter pandoc-citeproc -B title.md -B declaration.md final_report.md appendix.md -H preamble.tex --variable classoption=twocolumn --bibliography=final_report.bib --output final_report.md.pdf
+pandoc --toc --filter pandoc-xnos --filter pandoc-citeproc -B title.md -B declaration.md final_report.md appendix.md -H preamble.tex --bibliography=final_report.bib --output final_report.md.pdf
