e8b31ad2cfcd928ebb83134c8c261ee156ddab59
Hugh <hugh@crablab.co.uk>
2020-04-16 10:32:35 +0100

    Move to IEEE, fix some references and rewrite a few sections

--- reports/final_report/generate.sh
+++ reports/final_report/generate.sh
@@ -1,1 +1,1 @@
-pandoc --toc --filter pandoc-xnos --filter pandoc-citeproc -B title.md -B declaration.md final_report.md appendix.md user_guide.md -H preamble.tex -V geometry:margin=1in --bibliography=final_report.bib --output final_report.md.pdf
+pandoc --toc --filter pandoc-xnos --filter pandoc-citeproc --csl assets/ieee.csl -B title.md -B declaration.md final_report.md appendix.md user_guide.md -H preamble.tex -V geometry:margin=1in --bibliography=final_report.bib --output final_report.md.pdf
